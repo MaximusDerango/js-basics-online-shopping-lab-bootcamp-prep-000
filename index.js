b9121c2204cbc843e00ccb952a99f7354120172d
Maxim Dalton <maximcdalton@gmail.com>
2018-05-30 21:38:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,4 +57,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  if (cardNumber is null) {
+    return "Sorry, we don't have a credit card on file for you.";
+  }
 }
