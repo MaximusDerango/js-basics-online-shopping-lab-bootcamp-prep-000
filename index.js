d45c1777f5f1ea02fe5ec576e09aa0b5eb75abf3
Maxim Dalton <maximcdalton@gmail.com>
2018-05-30 21:36:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function removeFromCart(item) {
   // write your code here
   for (let i = 0; i < cart.length; i++) {
     if (item === cart[i].itemName) {
-      cart.splice(i, 0);
+      cart.splice(i, 1);
       return cart;
     }
   }
